31c959704f48b87bdd8a7ffe4894e050945261ac
gianlvalentini <gian.valentini1997@gmail.com>
2018-05-28 22:31:03 -0300

    arreglos

--- src/main/java/general/Administrador.java
+++ src/main/java/general/Administrador.java
@@ -9,14 +9,15 @@ public class Administrador extends Usuario {
 	
 	 int adminID;
 	 Date fechaDeAlta;
-	
-	public Administrador(String nombre, String ap, String userName, String password, int numeroId)
+
+	// Constructor
+public Administrador(String nombre, String ap, String userName, String password, int numeroId)
 	{
 		super(nombre,ap,userName,password);
 		this.adminID = numeroId;
 	}
 
-	public int mesesAdministrando() {
+public int mesesAdministrando() {
 		
 		DateTime hoy = new DateTime();
 		DateTime fechaAlta = new DateTime(fechaDeAlta);		
--- src/main/java/general/Cliente.java
+++ src/main/java/general/Cliente.java
@@ -9,111 +9,112 @@ import java.util.stream.Collectors;
 
 
 
-public class Cliente extends Usuario {
-	
-		String tipoDocumento;
-		int documento;
-		int telefonoContacto;
-	    String domicilio;
-		List<Dispositivo> dispositivos;
-		Categoria categoria;
-		int puntos;
+public class Cliente extends Usuario
+{
+    int puntos;
+    String tipoDocumento;
+    int documento;
+    int telefonoContacto;
+    String domicilio;
+    Categoria categoria;
+    List<Dispositivo> dispositivos;
+
 		
 		//fechaDeAlta;
-		//categoria
 		
 		// Constructor
 public Cliente(String nom, String ap, String nomUsuario, String contra, String direccion,String tipoDoc,int doc, int tel)
-	{
+{
 	super(nom,ap,nomUsuario,contra);
 	tipoDocumento = tipoDoc;
 	documento = doc;
 	telefonoContacto = tel;
 	domicilio = direccion;
 	puntos = 0;
-	}
+}
 
 
 public int cantDispositivosTotal() 
-	{
+{
 	return dispositivos.size();
-	}
+}
 
 public int cantDispositivosEncendidos() 
-	{
+{
 	return dispositivosEncendidos().size();
-	}
+}
 
 public int cantDispositivosApagados() 
-	{
+{
 	return cantDispositivosTotal() - cantDispositivosEncendidos();
-	}
+}
 
 	//Adaptar estos metodos
 
 public List<Dispositivo> dispositivosEncendidos() 
-	{
+{
 	return dispositivos.stream().filter(unDispositivo -> unDispositivo.estaEncendido())
 			 .collect(Collectors.toList());
-	}
+}
 
 public boolean algunoEncendido() 
 {
-	return dispositivos.stream().anyMatch(unDispositivo->unDispositivo.estaEncendido());
-
+    return dispositivos.stream().anyMatch(unDispositivo->unDispositivo.estaEncendido());
 }
 
-	public float consumoMensual() {
+public float consumoMensual()
+{
+    float consumo = 0;
 
-		float consumo = 0;
-		
-		for(Dispositivo dispositivo:dispositivos)
+    for(Dispositivo dispositivo:dispositivos)
 			consumo+= dispositivo.getConsumo();
-			
-		return consumo;
-	}
+
+    return consumo;
+
+}
 	
-	public float estimarFacturacion(List<Categoria> categorias){
+public float estimarFacturacion(List<Categoria> categorias){
 		return 	categoria.estimarFacturacionMensual(this);
 	}
 	
-	public void setCategoria(List<Categoria> categorias)
-	{
+public void setCategoria(List<Categoria> categorias)
+{
 	
-		categorias.sort(Comparator.comparingDouble(Categoria::consumoMensualMin)
+    categorias.sort(Comparator.comparingDouble(Categoria::consumoMensualMin)
                 );
 		
-		this.categoria = (categorias.stream()
-									.filter(c->c.pertence(this)).findFirst()
-									.get());					
-	}
+    this.categoria = (categorias.stream()
+            .filter(c->c.pertence(this)).findFirst()
+            .get());
+}
 
 
 
-	public Categoria getCategoria() {
+public Categoria getCategoria()
+{
 		return this.categoria;
-	}
+}
 
 
 
-	public String nombreCategoria() {
-	
+public String nombreCategoria()
+{
 		return categoria.tipo();
-	}
+}
 
 
 
-	public void agregarDispositivo(Dispositivo nuevoDispositivo)
-	{
+public void agregarDispositivo(Dispositivo nuevoDispositivo)
+{
 		dispositivos.add(nuevoDispositivo);
 		if(!serialRepetida(nuevoDispositivo.nroSerial()))
             puntos += nuevoDispositivo.puntos();
-	}
+}
 
-	public boolean serialRepetida(int serial_nueva)
-    {
+public boolean serialRepetida(int serial_nueva)
+{
         return dispositivos.stream().anyMatch(unDispositivo->(unDispositivo.nroSerial() == serial_nueva));
-    }
+}
 	
 }
 
