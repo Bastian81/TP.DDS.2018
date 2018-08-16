8386b50c248e3e8cecd02dfe953a807eb6e5bbe2
Joaquín Santos <jqnsantosr@gmail.com>
2018-08-15 22:06:16 -0300

    Main de ejemplo

---
+++ src/test/java/tests/MainTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import dispositivo.Dispositivo;
+import dispositivo.Inteligente;
+import estado.Ahorro;
+import estado.Apagado;
+import estado.Encendido;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        String s = "Hola";
+        System.out.println(s);
+
+        Ahorro estadoAhorro = new Ahorro();
+
+    }
+
+
+
+}
